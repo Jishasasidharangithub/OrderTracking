9b9a7f00f08bda3bb88916c8e22d536a5802d548
Jisha <jisha@meridian.net.in>
2023-07-19 18:28:20 +0530

    Initial commit

---
+++ app/src/main/java/com/jisha/ordertracking/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.jisha.ordertracking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jisha.ordertracking.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var binding : ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+    }
+}
